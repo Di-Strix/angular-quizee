95729466e33506e623c7f3a502e5ddcef63c06ae
Di-Strix <redstoniska.minecraft@gmail.com>
2022-03-18 22:31:45 +0200

    add comments to the firebase config

--- src/environments/firebase.config.prod.ts
+++ src/environments/firebase.config.prod.ts
@@ -1,3 +1,6 @@
+// execute the command below to stop tracking this file: 
+// git update-index --skip-worktree ./src/environments/firebase.config.prod.ts
+
 export const firebase = {
   projectId: '',
   appId: '',
--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,3 +1,6 @@
+// execute the command below to stop tracking this file: 
+// git update-index --skip-worktree ./src/environments/firebase.config.ts
+
 export const firebase = {
   projectId: '',
   appId: '',
