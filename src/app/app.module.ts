eabfe0ed15518b4bca817cc891be0c819c981a18
Di-Strix <redstoniska.minecraft@gmail.com>
2022-03-21 17:34:33 +0100

    format according to the latest style guides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ScreenTrackingService, UserTrackingService, getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { ReCaptchaV3Provider, initializeAppCheck, provideAppCheck } from '@angular/fire/app-check';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import {
-  provideAnalytics,
-  getAnalytics,
-  ScreenTrackingService,
-  UserTrackingService,
-} from '@angular/fire/analytics';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideDatabase, getDatabase } from '@angular/fire/database';
-import { provideFunctions, getFunctions } from '@angular/fire/functions';
-import { initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,11 +24,13 @@ import { initializeAppCheck, provideAppCheck, ReCaptchaV3Provider } from '@angul
     provideFunctions(() => getFunctions()),
     provideAppCheck(() => {
       const provider = new ReCaptchaV3Provider(environment.firebase.reCAPTCHAv3Token);
-      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+      return initializeAppCheck(undefined, {
+        provider,
+        isTokenAutoRefreshEnabled: true,
+      });
     }),
   ],
   providers: [ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
