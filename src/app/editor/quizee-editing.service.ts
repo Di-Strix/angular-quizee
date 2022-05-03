692de14c32bfe7efacd40c73b2d4f8a40d23f3b4
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-03 15:49:19 +0200

    Fix QuizeeEditingService::modify
    * Use recursive partial which allows optional nested types
    * use lodash::merge to deep merge changes

--- src/app/editor/quizee-editing.service.ts
+++ src/app/editor/quizee-editing.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Quiz } from '@di-strix/quizee-types';
 
+import * as _ from 'lodash';
 import { Observable, ReplaySubject } from 'rxjs';
+
+type RecursivePartial<T> = {
+  [K in keyof T]?: RecursivePartial<T[K]>;
+};
 
 @Injectable()
 export class QuizeeEditingService {
@@ -30,8 +35,8 @@ export class QuizeeEditingService {
     });
   }
 
-  modify(changes: Partial<Quiz>): Observable<Quiz> {
-    return this.load(Object.assign({}, this.quizee, changes));
+  modify(changes: RecursivePartial<Quiz>): Observable<Quiz> {
+    return this.load(_.merge({}, this.quizee, changes));
   }
 
   get(): Observable<Quiz> {
