35fdeafc59cf48f961d201601319ec59ff0db365
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-04 19:11:09 +0200

    refactor OverviewComponent

--- src/app/editor/overview/overview.component.ts
+++ src/app/editor/overview/overview.component.ts
@@ -1,7 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Quiz } from '@di-strix/quizee-types';
-
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 import { QuizeeEditingService } from '../quizee-editing.service';
 
@@ -10,10 +7,6 @@ import { QuizeeEditingService } from '../quizee-editing.service';
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent implements OnInit, OnDestroy {
+export class OverviewComponent {
   constructor(public quizeeEditingService: QuizeeEditingService) {}
-
-  ngOnInit(): void {}
-
-  ngOnDestroy(): void {}
 }
