5741e27a6c54c1090834d00047943f3a648e7328
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-05 17:34:01 +0200

    create BindToQuizee helper

---
+++ src/app/shared/helpers/BindToQuizee.ts
@@ -0,0 +1,24 @@
+import { FormControl } from '@angular/forms';
+
+import { Observable, Subscription, distinctUntilChanged, first } from 'rxjs';
+import { QuestionPair, QuizeeEditingService } from 'src/app/editor/quizee-editing.service';
+
+import { RecursivePartial } from './RecursivePartial';
+
+export interface BindToQuizeeModifiers<T> {
+  getter: () => Observable<T>;
+  setter: (changes: RecursivePartial<T>) => any;
+  dataParser: (data: T) => string;
+  quizeeModifier: (value: string) => RecursivePartial<T>;
+}
+
+export const bindToQuizee = <T>(formControl: FormControl, opts: BindToQuizeeModifiers<T>): Subscription => {
+  opts
+    .getter()
+    .pipe(first())
+    .subscribe((data) => formControl.setValue(opts.dataParser(data)));
+
+  return formControl.valueChanges
+    .pipe(distinctUntilChanged())
+    .subscribe((current) => opts.setter(opts.quizeeModifier(current)));
+};
