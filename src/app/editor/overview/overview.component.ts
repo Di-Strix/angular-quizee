17e586684af090fb8b02a45ba87ced2466399a1e
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-05 19:15:46 +0200

    add overview card enter and leave animations

--- src/app/editor/overview/overview.component.ts
+++ src/app/editor/overview/overview.component.ts
@@ -1,4 +1,6 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Question } from '@di-strix/quizee-types';
 
 import { QuizeeEditingService } from '../quizee-editing.service';
 
@@ -6,7 +8,20 @@ import { QuizeeEditingService } from '../quizee-editing.service';
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
+  animations: [
+    trigger('card', [
+      transition(':enter', [
+        style({ transform: 'scale(0)' }),
+        animate('.25s ease-out', style({ transform: 'scale(1)' })),
+      ]),
+      transition(':leave', animate('.25s ease-in', style({ transform: 'scale(0)' }))),
+    ]),
+  ],
 })
 export class OverviewComponent {
   constructor(public quizeeEditingService: QuizeeEditingService) {}
+
+  trackBy(_: number, item: Question) {
+    return item.id;
+  }
 }
