1ad23cd9e2aa0ac9c90d197c3c5dd5722f7a2b55
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-08 14:42:20 +0200

    add matchMedia mock

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,15 @@
 import 'jest-preset-angular/setup-jest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: any) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
