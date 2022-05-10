b9492d64b2633ed71de71663aef088a492b793dd
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-10 13:07:23 +0200

    update jest config adding coverage coverage threshold

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
