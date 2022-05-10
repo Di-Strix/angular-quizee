2ed364fac16ab47522d63bf8c713065fdae24722
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-10 15:36:55 +0200

    disable jest test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -4,14 +4,6 @@ const config: Config.InitialOptions = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
 
 export default config;
