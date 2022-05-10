7c0dc4711a7cbbcc7494cd229c9ca10442a3f29d
Di-Strix <redstoniska.minecraft@gmail.com>
2022-05-10 12:08:31 +0200

    implement answer-input

--- src/app/editor/settings/answer-input/answer-input.component.spec.ts
+++ src/app/editor/settings/answer-input/answer-input.component.spec.ts
@@ -1,26 +1,216 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import * as _ from 'lodash';
+import { Subject, of } from 'rxjs';
+
 import { EditorModule } from '../../editor.module';
+import { QuestionPair, QuizeeEditingService } from '../../quizee-editing.service';
 
 import { AnswerInputComponent } from './answer-input.component';
 
 describe('AnswerInputComponent', () => {
   let component: AnswerInputComponent;
   let fixture: ComponentFixture<AnswerInputComponent>;
+  let service: QuizeeEditingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AnswerInputComponent],
       imports: [EditorModule],
+      providers: [QuizeeEditingService],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AnswerInputComponent);
+    service = TestBed.inject(QuizeeEditingService);
     component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to current question', () => {
+    const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+    getCurrentQuestion.mockReturnValue(of());
+    component.ngOnInit();
+
+    expect(service.getCurrentQuestion).toHaveBeenCalled();
+  });
+
+  describe('answer', () => {
+    describe('input value change', () => {
+      it('should update quizee on input value change', () => {
+        jest.useFakeTimers();
+
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(
+          of({
+            answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
+            question: {
+              answerOptions: [],
+              caption: '',
+              id: 'question id',
+              type: 'WRITE_ANSWER',
+            },
+          } as QuestionPair)
+        );
+
+        const setAnswer = jest.spyOn(service, 'setAnswer');
+
+        component.ngOnInit();
+        component.answer.setValue('Some correct answer');
+
+        jest.runAllTimers();
+
+        expect(setAnswer).toHaveBeenCalledTimes(1);
+        expect(setAnswer.mock.calls[0][0]).toEqual(['Some correct answer']);
+      });
+    });
+
+    describe('form update event filtering', () => {
+      let mockQuestionPair: QuestionPair;
+
+      beforeEach(() => {
+        jest.useFakeTimers();
+
+        mockQuestionPair = {
+          answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
+          question: {
+            answerOptions: [],
+            caption: '',
+            id: 'question id',
+            type: 'WRITE_ANSWER',
+          },
+        };
+      });
+
+      it('should update form if answer changed', () => {
+        mockQuestionPair.answer.answer = ['some value'];
+
+        const subject = new Subject();
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(subject as any);
+
+        component.ngOnInit();
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        expect(component.answer.value).toBe('some value');
+        mockQuestionPair.answer.answer = ['some another value'];
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        expect(component.answer.value).toBe('some another value');
+      });
+
+      it('should not update form if value is the same', () => {
+        mockQuestionPair.answer.answer = ['val'];
+
+        const subject = new Subject();
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(subject as any);
+
+        component.ngOnInit();
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        const setValue = jest.spyOn(component.answer, 'setValue');
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        expect(setValue).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('config', () => {
+    describe('config change', () => {
+      it('should update quizee on config change', () => {
+        jest.useFakeTimers();
+
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(
+          of({
+            answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
+            question: {
+              answerOptions: [],
+              caption: '',
+              id: 'question id',
+              type: 'WRITE_ANSWER',
+            },
+          } as QuestionPair)
+        );
+
+        const setAnswerConfig = jest.spyOn(service, 'setAnswerConfig');
+
+        component.ngOnInit();
+        component.config.get('equalCase')?.setValue(true);
+
+        jest.runAllTimers();
+
+        expect(setAnswerConfig).toHaveBeenCalledTimes(1);
+        expect(setAnswerConfig.mock.calls[0][0].equalCase).toEqual(true);
+      });
+    });
+
+    describe('form update event filtering', () => {
+      let mockQuestionPair: QuestionPair;
+
+      beforeEach(() => {
+        jest.useFakeTimers();
+
+        mockQuestionPair = {
+          answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
+          question: {
+            answerOptions: [],
+            caption: '',
+            id: 'question id',
+            type: 'WRITE_ANSWER',
+          },
+        };
+      });
+
+      it('should update form if related config changed', () => {
+        mockQuestionPair.answer.config.equalCase = true;
+        const subject = new Subject();
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(subject as any);
+
+        component.ngOnInit();
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        expect(component.config.value.equalCase).toBe(true);
+        mockQuestionPair.answer.config.equalCase = false;
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        expect(component.config.value.equalCase).toBe(false);
+      });
+
+      it('should not update form if value is the same', () => {
+        mockQuestionPair.answer.config.equalCase = true;
+
+        const subject = new Subject();
+        const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
+        getCurrentQuestion.mockReturnValue(subject as any);
+
+        component.ngOnInit();
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        jest.runAllTimers();
+
+        const setValue = jest.spyOn(component.config, 'setValue');
+        subject.next(_.cloneDeep(mockQuestionPair));
+
+        expect(setValue).not.toHaveBeenCalled();
+      });
+    });
   });
 });
--- src/app/editor/settings/answer-input/answer-input.component.ts
+++ src/app/editor/settings/answer-input/answer-input.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Answer } from '@di-strix/quizee-types';
+
+import * as _ from 'lodash';
+import { Subscription, filter } from 'rxjs';
+import { RecursivePartial } from 'src/app/shared/helpers/RecursivePartial';
+
+import { QuizeeEditingService } from '../../quizee-editing.service';
+
+type Config = {
+  [P in keyof Answer['config']]: FormControl;
+};
 
 @Component({
   selector: 'app-answer-input',
   templateUrl: './answer-input.component.html',
-  styleUrls: ['./answer-input.component.scss']
+  styleUrls: ['./answer-input.component.scss'],
 })
-export class AnswerInputComponent implements OnInit {
+export class AnswerInputComponent implements OnInit, OnDestroy {
+  subs = new Subscription();
 
-  constructor() { }
+  answer = new FormControl('', [Validators.required]);
+  config = new FormGroup({ equalCase: new FormControl(false) } as Config);
+
+  constructor(public quizeeEditingService: QuizeeEditingService) {}
 
   ngOnInit(): void {
+    this.subs.add(
+      this.quizeeEditingService
+        .getCurrentQuestion()
+        .pipe(filter((qp) => qp.answer.answer[0] !== this.answer.value))
+        .subscribe((qp) => {
+          this.answer.setValue(qp.answer.answer[0]);
+        })
+    );
+
+    this.subs.add(
+      this.quizeeEditingService
+        .getCurrentQuestion()
+        .pipe(filter((qp) => !_.isEqual(qp.answer.config, this.config.value)))
+        .subscribe((qp) => {
+          this.config.controls['equalCase'].setValue(qp.answer.config.equalCase);
+        })
+    );
+
+    this.answer.valueChanges.subscribe((v) => {
+      this.quizeeEditingService.setAnswer([v]);
+    });
+
+    this.config.valueChanges.subscribe((v: Answer['config']) => {
+      this.quizeeEditingService.setAnswerConfig(v);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
