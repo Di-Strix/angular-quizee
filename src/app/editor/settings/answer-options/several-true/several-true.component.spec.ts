0997eb1b0f57e52fe7f964ec1bf18d660f011026
Di-Strix <redstoniska.minecraft@gmail.com>
2022-06-07 13:26:24 +0200

    update several-true component using selectiveAnswerBase

--- src/app/editor/settings/answer-options/several-true/several-true.component.spec.ts
+++ src/app/editor/settings/answer-options/several-true/several-true.component.spec.ts
@@ -1,10 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AnswerOption } from '@di-strix/quizee-types';
 
 import * as _ from 'lodash';
-import { Subject, of } from 'rxjs';
 import { EditorModule } from 'src/app/editor/editor.module';
-import { QuestionPair, QuizeeEditingService } from 'src/app/editor/quizee-editing.service';
+import { QuizeeEditingService } from 'src/app/editor/quizee-editing.service';
 
 import { SeveralTrueComponent } from './several-true.component';
 
@@ -25,227 +23,26 @@ describe('SeveralTrueComponent', () => {
     fixture = TestBed.createComponent(SeveralTrueComponent);
     service = TestBed.inject(QuizeeEditingService);
     component = fixture.componentInstance;
+
+    jest.useFakeTimers();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('assembleFunctions', () => {
-    let testComponent: {
-      assembleAnswers: SeveralTrueComponent['assembleAnswers'];
-      assembleAnswerOptions: SeveralTrueComponent['assembleAnswerOptions'];
-    };
-
-    beforeEach(() => {
-      testComponent = component as any;
-    });
-
-    describe('assembleAnswers', () => {
-      it('should work', () => {
-        const result = testComponent.assembleAnswers({
-          id1: { isCorrect: false, value: '' },
-          id2: { isCorrect: true, value: '' },
-        });
-
-        expect(result).toEqual(['id2']);
-      });
-    });
-
-    describe('assembleAnswerOptions', () => {
-      it('should work', () => {
-        const result = testComponent.assembleAnswerOptions({
-          id1: { isCorrect: false, value: 'answerOption1' },
-          id2: { isCorrect: false, value: 'answerOption2' },
-        });
-
-        expect(result).toEqual([
-          { id: 'id1', value: 'answerOption1' },
-          { id: 'id2', value: 'answerOption2' },
-        ]);
-      });
-    });
-  });
-
-  it('should subscribe to current question', () => {
-    const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-    getCurrentQuestion.mockReturnValue(of());
-    component.ngOnInit();
-
-    expect(getCurrentQuestion).toHaveBeenCalled();
-  });
-
-  it('should create form controls for each answer option', () => {
-    const ids = ['id1', 'id2', 'id3'];
-
-    const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-    getCurrentQuestion.mockReturnValue(
-      of({
-        answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
-        question: {
-          answerOptions: ids.map<AnswerOption>((id) => ({ id, value: 'id' })),
-          caption: '',
-          id: 'question id',
-          type: 'SEVERAL_TRUE',
-        },
-      } as QuestionPair)
-    );
+  it('should call subscribeToUpdates', () => {
+    const subscribeToUpdates = jest.spyOn(component, 'subscribeToUpdates');
 
     component.ngOnInit();
 
-    expect(Object.keys(component.formGroup.controls)).toEqual(expect.arrayContaining(ids));
-  });
-
-  describe('value change', () => {
-    it('should update quizee on input value change', () => {
-      jest.useFakeTimers();
-
-      const ids = ['id1'];
-
-      const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-      getCurrentQuestion.mockReturnValue(
-        of({
-          answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
-          question: {
-            answerOptions: ids.map<AnswerOption>((id) => ({ id, value: 'id' })),
-            caption: '',
-            id: 'question id',
-            type: 'SEVERAL_TRUE',
-          },
-        } as QuestionPair)
-      );
-      const setAnswer = jest.spyOn(service, 'setAnswer');
-      const setAnswerOptions = jest.spyOn(service, 'setAnswerOptions');
-
-      component.ngOnInit();
-      component.formGroup.controls['id1'].get('value')?.setValue('id1');
-
-      jest.runAllTimers();
-
-      expect(setAnswer).toHaveBeenCalledTimes(1);
-      expect(setAnswerOptions).toHaveBeenCalledTimes(1);
-
-      expect(setAnswer.mock.calls[0][0]).toEqual([]);
-      expect(setAnswerOptions.mock.calls[0][0]).toEqual([{ id: 'id1', value: 'id1' }]);
-    });
-
-    it('should update quizee on checkbox value change', () => {
-      jest.useFakeTimers();
-
-      const ids = ['id1'];
-
-      const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-      getCurrentQuestion.mockReturnValue(
-        of({
-          answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
-          question: {
-            answerOptions: ids.map<AnswerOption>((id) => ({ id, value: 'id' })),
-            caption: '',
-            id: 'question id',
-            type: 'SEVERAL_TRUE',
-          },
-        } as QuestionPair)
-      );
-      const setAnswer = jest.spyOn(service, 'setAnswer');
-      const setAnswerOptions = jest.spyOn(service, 'setAnswerOptions');
-
-      component.ngOnInit();
-      component.formGroup.controls['id1'].get('isCorrect')?.setValue(true);
-
-      jest.runAllTimers();
-
-      expect(setAnswer).toHaveBeenCalledTimes(1);
-      expect(setAnswerOptions).toHaveBeenCalledTimes(1);
-
-      expect(setAnswer.mock.calls[0][0]).toEqual(['id1']);
-      expect(setAnswerOptions.mock.calls[0][0]).toEqual([{ id: 'id1', value: 'id' }]);
-    });
-  });
-
-  describe('form re-init event filtering', () => {
-    let mockQuestionPair: QuestionPair;
-
-    beforeEach(() => {
-      jest.useFakeTimers();
-
-      mockQuestionPair = {
-        answer: { answer: [], answerTo: 'question id', config: { equalCase: false } },
-        question: {
-          answerOptions: [],
-          caption: '',
-          id: 'question id',
-          type: 'SEVERAL_TRUE',
-        },
-      };
-    });
-
-    it('should not re-init form group if values are the same', () => {
-      mockQuestionPair.question.answerOptions = [{ id: 'some-unique-id', value: '1' }];
-
-      const subject = new Subject();
-      const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-      getCurrentQuestion.mockReturnValue(subject as any);
-
-      component.ngOnInit();
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      const form = component.formGroup;
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      expect(component.formGroup === form).toBeTruthy();
-    });
-
-    it('should re-+init form group if answer options changed', () => {
-      mockQuestionPair.question.answerOptions = [{ id: 'some-unique-id', value: '1' }];
-
-      const subject = new Subject();
-      const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-      getCurrentQuestion.mockReturnValue(subject as any);
-
-      component.ngOnInit();
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      const form = component.formGroup;
-      mockQuestionPair.question.answerOptions[0].value = '2';
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      expect(component.formGroup === form).toBeFalsy();
-    });
-
-    it('should re-init form group if answers changed', () => {
-      mockQuestionPair.question.answerOptions = [{ id: 'some-unique-id', value: '1' }];
-
-      const subject = new Subject();
-      const getCurrentQuestion = jest.spyOn(service, 'getCurrentQuestion');
-      getCurrentQuestion.mockReturnValue(subject as any);
-
-      component.ngOnInit();
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      const form = component.formGroup;
-      mockQuestionPair.answer.answer.push('1');
-      subject.next(_.cloneDeep(mockQuestionPair));
-
-      jest.runAllTimers();
-
-      expect(component.formGroup === form).toBeFalsy();
-    });
+    expect(subscribeToUpdates).toBeCalledTimes(1);
   });
 
   describe('remove answer option', () => {
     it('should not remove answer option if it is the last one', () => {
       const removeAnswerOption = jest.spyOn(service, 'removeAnswerOption');
-      component.ids = ['1'];
+      component.controls = [{ id: '1', control: {} as any }];
       component.removeAnswerOption('1');
 
       jest.runAllTimers();
@@ -255,7 +52,10 @@ describe('SeveralTrueComponent', () => {
 
     it('should prompt service to remove answer option with provided id', () => {
       const removeAnswerOption = jest.spyOn(service, 'removeAnswerOption');
-      component.ids = ['1', '2'];
+      component.controls = [
+        { id: '1', control: {} as any },
+        { id: '2', control: {} as any },
+      ];
       component.removeAnswerOption('1');
 
       jest.runAllTimers();
@@ -273,6 +73,37 @@ describe('SeveralTrueComponent', () => {
       jest.runAllTimers();
 
       expect(addAnswerOption).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('setAnswer', () => {
+    it('should allow multiple correct answer', () => {
+      component.setAnswer('1');
+      component.setAnswer('2');
+      component.setAnswer('3');
+
+      expect(component.correctAnswers).toEqual(['1', '2', '3']);
+    });
+
+    it('should toggle correct answer ids', () => {
+      component.setAnswer('1');
+      component.setAnswer('2');
+      component.setAnswer('3');
+
+      component.setAnswer('1');
+
+      expect(component.correctAnswers).toEqual(['2', '3']);
+    });
+
+    it('should push answer to service', () => {
+      const setAnswer = jest.spyOn(service, 'setAnswer');
+
+      component.setAnswer('1');
+      component.setAnswer('2');
+      component.setAnswer('3');
+
+      expect(setAnswer).toBeCalledTimes(3);
+      expect(setAnswer.mock.calls[2][0]).toEqual(['1', '2', '3']);
     });
   });
 });
--- src/app/editor/settings/answer-options/several-true/several-true.component.ts
+++ src/app/editor/settings/answer-options/several-true/several-true.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 import { AnswerOption, AnswerOptionId } from '@di-strix/quizee-types';
 
 import * as _ from 'lodash';
 import { Subscription, filter } from 'rxjs';
 import { QuizeeEditingService } from 'src/app/editor/quizee-editing.service';
+
+import { SelectiveAnswersBase } from '../selective-answers-base.component';
 
 export interface SeveralTrueAnswerOptionForm {
   [key: string]: {
@@ -18,67 +21,34 @@ export interface SeveralTrueAnswerOptionForm {
   templateUrl: './several-true.component.html',
   styleUrls: ['./several-true.component.scss'],
 })
-export class SeveralTrueComponent implements OnInit, OnDestroy {
-  ids: AnswerOptionId[] = [];
-  formGroup = new FormGroup({});
-
+export class SeveralTrueComponent extends SelectiveAnswersBase implements OnInit, OnDestroy {
   subs = new Subscription();
 
-  constructor(public quizeeEditingService: QuizeeEditingService) {}
+  constructor(public quizeeEditingService: QuizeeEditingService) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.subs.add(
-      this.quizeeEditingService
-        .getCurrentQuestion()
-        .pipe(
-          filter((questionPair) => {
-            return (
-              !_.isEqual(this.assembleAnswers(this.formGroup.value), questionPair.answer.answer) ||
-              !_.isEqual(this.assembleAnswerOptions(this.formGroup.value), questionPair.question.answerOptions)
-            );
-          })
-        )
-        .subscribe((pair) => {
-          this.ids = [];
-          this.formGroup = new FormGroup({});
-
-          this.ids = pair.question.answerOptions.map((answerOption) => {
-            const questionFormGroup = new FormGroup({
-              value: new FormControl(answerOption.value, [Validators.required]),
-              isCorrect: new FormControl(pair.answer.answer.includes(answerOption.id)),
-            });
-            this.formGroup.addControl(answerOption.id, questionFormGroup);
-
-            return answerOption.id;
-          });
-
-          this.formGroup.valueChanges.subscribe((object: SeveralTrueAnswerOptionForm) => {
-            this.quizeeEditingService.setAnswer(this.assembleAnswers(object));
-            this.quizeeEditingService.setAnswerOptions(this.assembleAnswerOptions(object));
-          });
-        })
-    );
+    this.subs.add(this.subscribeToUpdates(this.quizeeEditingService));
   }
 
   ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
 
+  setAnswer(id: AnswerOptionId) {
+    this.correctAnswers = _.xor(this.correctAnswers, [id]);
+
+    this.quizeeEditingService.setAnswer(this.assembleAnswers());
+  }
+
   removeAnswerOption(id: AnswerOptionId) {
-    if (this.ids.length <= 1) return;
+    if (this.controls.length <= 1) return;
 
     this.quizeeEditingService.removeAnswerOption(id);
   }
 
   createAnswerOption() {
     this.quizeeEditingService.addAnswerOption();
-  }
-
-  private assembleAnswers(object: SeveralTrueAnswerOptionForm): AnswerOptionId[] {
-    return Object.keys(object).reduce((acc, key) => (object[key].isCorrect ? [...acc, key] : acc), Array());
-  }
-
-  private assembleAnswerOptions(object: SeveralTrueAnswerOptionForm): AnswerOption[] {
-    return Object.keys(object).reduce((acc, key) => [...acc, { id: key, value: object[key].value }], Array());
   }
 }
