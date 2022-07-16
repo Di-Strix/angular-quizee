430cc8bac51332232922c84f9ab19fd8eda5585e
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-07-16 10:31:42 +0200

    create and implement auth-dialog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { AuthDialogComponent } from './components/auth-dialog/auth-dialog.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [AuthDialogComponent],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  exports: [AuthDialogComponent],
 })
 export class SharedModule {}
