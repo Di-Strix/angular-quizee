54c69a0f2fad108c3801cb1db998b0ba30045f86
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-07-16 10:33:55 +0200

    implement auth guard in quizee service
    Add auth guards before actual invocations of some functions

--- src/app/shared/services/quizee.service.spec.ts
+++ src/app/shared/services/quizee.service.spec.ts
@@ -1,112 +1,165 @@
 import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Functions } from '@angular/fire/functions';
+import { MatDialog } from '@angular/material/dialog';
 
-import { of, throwError } from 'rxjs';
+import { httpsCallable } from 'firebase/functions';
+import { Subject, of, throwError } from 'rxjs';
 
+import { AuthService } from './auth.service';
 import { QuizeeService } from './quizee.service';
 
+jest.mock('firebase/functions');
+jest.mock('@angular/fire/firestore');
+jest.mock('@angular/material/dialog');
+jest.mock('./auth.service');
+
+const mockCloudFunction = (expectedFnName: string) => {
+  const fn = jest.fn();
+
+  (httpsCallable as jest.Mock).mockImplementation((_, calledFnName) => (...args: any) => {
+    if (calledFnName === expectedFnName) return fn(...args);
+    else throw new Error(`expected ${expectedFnName} to be called, but got ${calledFnName} called`);
+  });
+
+  return fn;
+};
+
 describe('QuizeeService', () => {
-  let mockDB: object = {};
+  let mockDB: any = {};
   let service: QuizeeService;
+  let authService: AuthService;
+  let matDialog: MatDialog;
+  let next: jest.Mock;
+  let error: jest.Mock;
 
   beforeEach(() => {
-    // @ts-ignore
-    doc = jest.fn((anchor = mockDB, path: string) => {
+    (doc as jest.Mock).mockImplementation((anchor = mockDB, path: string) => {
       path.split('/').forEach((key: string) => (anchor = anchor && key in anchor ? anchor[key] : undefined));
       return anchor;
     });
 
-    // @ts-ignore
-    // noinspection JSConstantReassignment
-    docData = jest.fn((val) => val);
+    (docData as jest.Mock).mockImplementation((val) => val);
 
     mockDB = {};
-    service = new QuizeeService(undefined as any as Firestore);
+
+    authService = new AuthService({} as any);
+    matDialog = new (MatDialog as any)();
+
+    service = new QuizeeService(undefined as any as Firestore, {} as Functions, authService, matDialog);
+
+    jest.spyOn(service, 'withAuthGuard' as any).mockImplementation((fn: any) => fn());
+
+    next = jest.fn();
+    error = jest.fn();
+
+    jest.useFakeTimers();
   });
 
-  describe('#getQuizee', () => {
-    it('should be created', () => {
-      expect(service).toBeTruthy();
+  describe('getQuizee', () => {
+    it('should be called with auth guard', async () => {
+      mockDB = { quizees: { 1: of({}) } };
+
+      service.getQuizee('1').subscribe();
+
+      await jest.runAllTimers();
+
+      expect((service as any).withAuthGuard).toBeCalledTimes(1);
     });
 
-    it(`should throw error if data is undefined`, (done) => {
+    it(`should throw error if data is undefined`, async () => {
       mockDB = { quizees: { mockId: of(undefined) } };
-      const error = () => done();
 
-      service.getQuizee('mockId').subscribe({ error });
+      service.getQuizee('mockId').subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(next).not.toBeCalled();
+      expect(error).toBeCalledTimes(1);
     });
 
-    it(`should push Quiz to subscriber`, () => {
-      expect.assertions(1);
-
+    it(`should push Quiz to subscriber`, async () => {
       const symbol = Symbol();
+      const mockData = {
+        [symbol]: 1,
+      };
       mockDB = {
         quizees: {
-          mockId: of({
-            [symbol]: 1,
-          }),
+          mockId: of(mockData),
         },
       };
 
-      const next = (val: any) => expect(val[symbol]).toBe(1);
+      service.getQuizee('mockId').subscribe({ next, error });
 
-      service.getQuizee('mockId').subscribe({ next });
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledWith(mockData);
     });
 
-    it(`should push Quiz to subscriber only once if once = true`, () => {
-      expect.assertions(1);
-
+    it(`should push Quiz to subscriber only once if once = true`, async () => {
       mockDB = {
         quizees: {
           mockArr: of(1, 2, 3),
         },
       };
 
-      const next = (_: any) => expect(true).toBeTruthy();
+      service.getQuizee('mockArr', true).subscribe({ next, error });
 
-      service.getQuizee('mockArr', true).subscribe({ next });
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
     });
 
-    it(`should push Quiz to subscriber everytime it updates if once = false`, () => {
-      expect.assertions(3);
-
+    it(`should push Quiz to subscriber every time it updates if once = false`, async () => {
       mockDB = {
         quizees: {
           mockArr: of(1, 2, 3),
         },
       };
 
-      const next = (_: any) => expect(true).toBeTruthy();
+      service.getQuizee('mockArr').subscribe({ next, error });
 
-      service.getQuizee('mockArr').subscribe({ next });
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(3);
     });
   });
 
-  describe('#getQuizeePublicData', () => {
-    it('should return Quiz', () => {
-      expect.assertions(3);
-
+  describe('getQuizeePublicData', () => {
+    it('should return Quiz', async () => {
       const infoSymbol = Symbol();
       const questionsSymbol = Symbol();
+
+      const mockData = {
+        info: { [infoSymbol]: 1 },
+        questions: { [questionsSymbol]: 1 },
+        answers: [],
+      };
 
       mockDB = {
         quizees: {
           mock: {
-            info: of({ [infoSymbol]: 1 }),
-            questions: of({ [questionsSymbol]: 1 }),
+            info: of(mockData.info),
+            questions: of(mockData.questions),
           },
         },
       };
 
       service.getQuizeePublicData('mock').subscribe({
-        next: (value: any) => {
-          expect(value.info[infoSymbol]).toBe(1);
-          expect(value.questions[questionsSymbol]).toBe(1);
-          expect(value.answers).toBeTruthy();
-        },
+        next,
+        error,
       });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith(mockData);
     });
 
-    it('should not try to access answers', (done) => {
+    it('should not try to access answers', async () => {
       mockDB = {
         quizees: {
           mock: {
@@ -117,13 +170,16 @@ describe('QuizeeService', () => {
         },
       };
 
-      service.getQuizeePublicData('mock').subscribe({ next: (_) => done() });
+      service.getQuizeePublicData('mock').subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
     });
 
-    it('should throw error if info or questions is undefined', () => {
-      expect.assertions(3);
-
-      const performTest = (info: any, questions: any) => {
+    it('should throw error if info or questions is undefined', async () => {
+      const performTest = async (info: any, questions: any) => {
         mockDB = {
           quizees: {
             mock: {
@@ -133,15 +189,20 @@ describe('QuizeeService', () => {
           },
         };
 
-        service.getQuizeePublicData('mock').subscribe({ error: () => expect(true).toBeTruthy() });
+        service.getQuizeePublicData('mock').subscribe({ next, error });
+
+        await jest.runAllTimers();
+
+        expect(next).not.toBeCalled();
+        expect(error).toBeCalled();
       };
 
-      performTest({}, undefined);
-      performTest(undefined, []);
-      performTest(undefined, undefined);
+      await performTest({}, undefined);
+      await performTest(undefined, []);
+      await performTest(undefined, undefined);
     });
 
-    it('should complete stream after getting data', (done) => {
+    it('should complete stream after getting data', async () => {
       mockDB = {
         quizees: {
           mock: {
@@ -151,7 +212,249 @@ describe('QuizeeService', () => {
         },
       };
 
-      service.getQuizeePublicData('mock').subscribe({ complete: () => done() });
+      const complete = jest.fn();
+
+      service.getQuizeePublicData('mock').subscribe({ next, error, complete });
+
+      await jest.runAllTimers();
+
+      expect(next).toBeCalledTimes(1);
+      expect(error).not.toBeCalled();
+      expect(complete).toBeCalled();
+    });
+  });
+
+  describe('getQuizeeList', () => {
+    let fn: jest.Mock;
+
+    beforeEach(() => {
+      fn = mockCloudFunction('getQuizeeList');
+      fn.mockImplementation(async () => ({ data: {} }));
+    });
+
+    it('should call cloud function', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.getQuizeeList().subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(fn).toBeCalledTimes(1);
+      expect(next).toBeCalledTimes(1);
+    });
+
+    it('should return cloud function response', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.getQuizeeList().subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith(mockData);
+    });
+  });
+
+  describe('checkAnswers', () => {
+    let fn: jest.Mock;
+
+    beforeEach(() => {
+      fn = mockCloudFunction('checkAnswers');
+      fn.mockImplementation(async () => ({ data: {} }));
+    });
+
+    it('should call cloud function', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.checkAnswers({} as any).subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(fn).toBeCalledTimes(1);
+      expect(next).toBeCalledTimes(1);
+    });
+
+    it('should return cloud function response', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.checkAnswers({} as any).subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith(mockData);
+    });
+  });
+
+  describe('publishQuizee', () => {
+    let fn: jest.Mock;
+
+    beforeEach(() => {
+      fn = mockCloudFunction('publishQuizee');
+      fn.mockImplementation(async () => ({ data: {} }));
+    });
+
+    it('should call cloud function', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.publishQuizee({} as any).subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(fn).toBeCalledTimes(1);
+      expect(next).toBeCalledTimes(1);
+    });
+
+    it('should return cloud function response', async () => {
+      const mockData = { someMockData: 'abc' };
+      fn.mockImplementation(() => Promise.resolve({ data: mockData }));
+
+      service.publishQuizee({} as any).subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith(mockData);
+    });
+
+    it('should be called with auth guard', async () => {
+      service.publishQuizee({} as any).subscribe();
+
+      await jest.runAllTimers();
+
+      expect((service as any).withAuthGuard).toBeCalledTimes(1);
+    });
+  });
+
+  describe('callHttpsFunction', () => {
+    it('should call function with provided args', async () => {
+      const fn = jest.fn().mockReturnValue(Promise.resolve(0));
+
+      (service as any).callHttpsFunction(fn, 1, 2, 3);
+
+      await jest.runAllTimers();
+
+      expect(fn).toBeCalledTimes(1);
+      expect(fn).toBeCalledWith(1, 2, 3);
+    });
+
+    it('should unwrap cloud function response', async () => {
+      const mockData = { 1: 'a', b: 2 };
+      const fn = jest.fn().mockReturnValue(Promise.resolve({ data: mockData }));
+
+      (service as any).callHttpsFunction(fn).subscribe({ next, error });
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith(mockData);
+    });
+  });
+
+  describe('withAuthGuard', () => {
+    let afterClosed$: Subject<boolean>;
+    let isAuthenticated$: Subject<boolean>;
+
+    beforeEach(() => {
+      afterClosed$ = new Subject<boolean>();
+      isAuthenticated$ = new Subject<boolean>();
+
+      ((service as any).withAuthGuard as jest.Mock).mockRestore();
+
+      jest.spyOn(matDialog, 'open').mockReturnValue({ afterClosed: () => afterClosed$ } as any);
+      jest.spyOn(authService, 'isAuthenticated').mockReturnValue(isAuthenticated$);
+    });
+
+    it('should request auth state from auth service', async () => {
+      (service as any).withAuthGuard(() => of()).subscribe();
+
+      isAuthenticated$.next(true);
+
+      await jest.runAllTimers();
+
+      expect(authService.isAuthenticated).toBeCalled();
+    });
+
+    it('should immediately switch to provided stream if authorized', async () => {
+      (service as any).withAuthGuard(() => of(1, 2, 3)).subscribe({ next, error });
+
+      isAuthenticated$.next(true);
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(3);
+
+      expect(authService.isAuthenticated).toBeCalled();
+    });
+
+    it('should open dialog and wait for auth if not authorized', async () => {
+      (service as any).withAuthGuard(() => of(1, 2, 3)).subscribe({ next, error });
+
+      isAuthenticated$.next(false);
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).not.toBeCalled();
+
+      expect(matDialog.open).toBeCalledTimes(1);
+      expect(authService.isAuthenticated).toBeCalled();
+    });
+
+    it('should throw if dialog closed before auth', async () => {
+      (service as any).withAuthGuard(() => of(1, 2, 3)).subscribe({ next, error });
+
+      isAuthenticated$.next(false);
+      afterClosed$.next(false);
+
+      await jest.runAllTimers();
+
+      expect(error).toBeCalledTimes(1);
+      expect(next).not.toBeCalled();
+    });
+
+    it('should close dialog and switch to provided stream when authorized after dialog open', async () => {
+      jest.spyOn(matDialog, 'closeAll');
+
+      (service as any).withAuthGuard(() => of(1, 2, 3)).subscribe({ next, error });
+
+      isAuthenticated$.next(false);
+      isAuthenticated$.next(true);
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(3);
+
+      expect(matDialog.closeAll).toBeCalledTimes(1);
+    });
+
+    it('should not start provided stream every time auth state changes', async () => {
+      (service as any).withAuthGuard(() => of(1, 2, 3)).subscribe({ next, error });
+
+      isAuthenticated$.next(true);
+
+      await jest.runAllTimers();
+
+      isAuthenticated$.next(true);
+
+      await jest.runAllTimers();
+
+      expect(error).not.toBeCalled();
+      expect(next).toBeCalledTimes(3);
     });
   });
 });
--- src/app/shared/services/quizee.service.ts
+++ src/app/shared/services/quizee.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Functions } from '@angular/fire/functions';
+import { MatDialog } from '@angular/material/dialog';
+import { CheckAnswers, GetQuizeeList, PublishQuizee } from '@di-strix/quizee-cloud-functions-interfaces';
 import { Question, Quiz, QuizId, QuizInfo } from '@di-strix/quizee-types';
 
-import { Observable, map, take, tap, zipWith } from 'rxjs';
+import { HttpsCallable, httpsCallable } from 'firebase/functions';
+import { Observable, filter, first, from, map, merge, of, switchMap, tap, throwError, zipWith } from 'rxjs';
+
+import { AuthDialogComponent } from '../components/auth-dialog/auth-dialog.component';
+
+import { AuthService } from './auth.service';
+
+type PromiseToObservable<T> = T extends Promise<infer R> ? Observable<R> : never;
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuizeeService {
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private cloudFunctions: Functions,
+    private authService: AuthService,
+    private authDialog: MatDialog
+  ) {}
 
   getQuizee(id: QuizId, once: boolean = false): Observable<Quiz> {
-    return docData<Quiz>(doc(this.firestore, `quizees/${id}`) as any).pipe(
-      once ? take(1) : tap(),
-      tap((data) => {
-        if (!data) throw new Error('Quizee with given id does not exist');
-      })
+    return this.withAuthGuard(() =>
+      docData<Quiz>(doc(this.firestore, `quizees/${id}`) as any).pipe(
+        once ? first() : tap(),
+        tap((data) => {
+          if (!data) throw new Error('Quizee with given id does not exist');
+        })
+      )
     );
   }
 
@@ -24,7 +41,7 @@ export class QuizeeService {
 
     return docData<QuizInfo>(doc(anchor, 'info') as any).pipe(
       zipWith(docData<Question[]>(doc(anchor, 'questions') as any)),
-      take(1),
+      first(),
       tap(([info, questions]: [QuizInfo, Question[]]) => {
         if (!info || !questions) throw new Error('Quizee with given id does not exist');
       }),
@@ -35,6 +52,45 @@ export class QuizeeService {
           questions,
         })
       )
+    );
+  }
+
+  getQuizeeList(...args: GetQuizeeList.Args): PromiseToObservable<GetQuizeeList.ReturnType> {
+    return this.callHttpsFunction(httpsCallable(this.cloudFunctions, 'getQuizeeList'), ...args);
+  }
+
+  checkAnswers(...args: CheckAnswers.Args): PromiseToObservable<CheckAnswers.ReturnType> {
+    return this.callHttpsFunction(httpsCallable(this.cloudFunctions, 'checkAnswers'), ...args);
+  }
+
+  publishQuizee(...args: PublishQuizee.Args): PromiseToObservable<PublishQuizee.ReturnType> {
+    return this.withAuthGuard(() =>
+      this.callHttpsFunction(httpsCallable(this.cloudFunctions, 'publishQuizee'), ...args)
+    );
+  }
+
+  private callHttpsFunction<ReturnType, Args>(fn: HttpsCallable, ...args: Args[]): Observable<ReturnType> {
+    return from(fn(...args)).pipe(map((res) => res.data as ReturnType));
+  }
+
+  private withAuthGuard<T>(fn: () => Observable<T>): Observable<T> {
+    return this.authService.isAuthenticated().pipe(
+      first(),
+      switchMap((state) =>
+        !state
+          ? merge(
+              this.authDialog
+                .open(AuthDialogComponent)
+                .afterClosed()
+                .pipe(map(() => false)),
+              this.authService.isAuthenticated().pipe(filter((v) => v))
+            )
+          : of(true)
+      ),
+      first(),
+      switchMap((state) => (!state ? throwError(() => new Error('Auth required')) : of(true))),
+      tap(() => this.authDialog.closeAll()),
+      switchMap(() => fn())
     );
   }
 }
