5cf0b16f9f4e55a0e4cb8f3c5e47fc4a5b401b0a
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-08-06 13:49:34 +0200

    add separate trigger for opacity transition

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -36,6 +36,20 @@ import { QuizeeService } from 'src/app/shared/services/quizee.service';
         animate('.3s ease-in', style({ opacity: 0 })),
       ]),
     ]),
+    trigger('opacityTransition', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+        }),
+        animate('.3s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 1,
+        }),
+        animate('.3s ease-in', style({ opacity: 0 })),
+      ]),
+    ]),
   ],
 })
 export class HomeComponent implements OnInit {
