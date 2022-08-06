ede033a6cf7878f7f785875509d95212c0d0cc02
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-08-06 12:46:51 +0200

    remove unused import

--- src/app/shared/directives/class-when-loaded.directive.ts
+++ src/app/shared/directives/class-when-loaded.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, TemplateRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: 'img[appClassWhenLoaded]',
