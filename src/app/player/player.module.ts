5bdcc805508a033b78448142b6e5a54c1b38345b
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-08-26 12:31:06 +0200

    add player routing module

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { PlayerRoutingModule } from './player-routing.module';
 import { PlayerComponent } from './player.component';
 import { QuizeeNotFoundComponent } from './quizee-not-found/quizee-not-found.component';
 
 @NgModule({
   declarations: [PlayerComponent, QuizeeNotFoundComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, PlayerRoutingModule, SharedModule],
 })
 export class PlayerModule {}
