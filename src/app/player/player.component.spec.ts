229ec417b7a0e4670be4b598c704b9aa98046e36
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-08-27 11:59:01 +0200

    update `PlayerComponent`

--- src/app/player/player.component.spec.ts
+++ src/app/player/player.component.spec.ts
@@ -1,22 +1,126 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Observable, Subject, Subscription, of, throwError } from 'rxjs';
 
 import { PlayerComponent } from './player.component';
+import { PlayerService } from './player.service';
+
+jest.mock('@angular/router');
+jest.mock('./player.service');
 
 describe('PlayerComponent', () => {
+  let playerService: jest.MockedClass<typeof PlayerService>['prototype'];
+  let activatedRoute: jest.MockedClass<typeof ActivatedRoute>['prototype'];
+  let router: jest.MockedClass<typeof Router>['prototype'];
   let component: PlayerComponent;
-  let fixture: ComponentFixture<PlayerComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [PlayerComponent],
-    }).compileComponents();
+    playerService = new (PlayerService as any)();
+    activatedRoute = new (ActivatedRoute as any)();
+    router = new (Router as any)();
 
-    fixture = TestBed.createComponent(PlayerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new PlayerComponent(playerService, activatedRoute, router);
+
+    jest.useFakeTimers();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onInit', () => {
+    it('should subscribe to paramMap', async () => {
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+
+      await jest.runAllTimers();
+
+      expect(subject.observed).toBeTruthy();
+    });
+
+    it('should get id from paramMap', async () => {
+      playerService.loadQuizee.mockReturnValue(of());
+      const get = jest.fn();
+
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+      subject.next({ get });
+
+      await jest.runAllTimers();
+
+      expect(get).toBeCalledTimes(1);
+      expect(get).toBeCalledWith('id');
+    });
+
+    it('should call loadQuizee with provided id', async () => {
+      playerService.loadQuizee.mockReturnValue(of());
+      const get = jest.fn().mockReturnValue('someId');
+
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+      subject.next({ get });
+
+      await jest.runAllTimers();
+
+      expect(playerService.loadQuizee).toBeCalledTimes(1);
+      expect(playerService.loadQuizee).toBeCalledWith('someId');
+    });
+
+    it('should place empty string if id is falsy', async () => {
+      playerService.loadQuizee.mockReturnValue(of());
+      const get = jest.fn();
+
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+      subject.next({ get });
+
+      await jest.runAllTimers();
+
+      expect(playerService.loadQuizee).toBeCalledTimes(1);
+      expect(playerService.loadQuizee).toBeCalledWith('');
+    });
+
+    it('should navigate to 404 if loadQuizee pushed error', async () => {
+      playerService.loadQuizee.mockReturnValue(throwError(() => new Error('mockError')));
+      const get = jest.fn();
+
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+      subject.next({ get });
+
+      await jest.runAllTimers();
+
+      expect(playerService.loadQuizee).toBeCalledTimes(1);
+      expect(router.navigate).toBeCalledTimes(1);
+      expect(router.navigate).toBeCalledWith(['solve/404']);
+    });
+  });
+
+  describe('onDestroy', () => {
+    it('should unsubscribe', async () => {
+      const subject = new Subject();
+      (activatedRoute as any).paramMap = subject;
+
+      component.ngOnInit();
+
+      await jest.runAllTimers();
+
+      component.ngOnDestroy();
+
+      await jest.runAllTimers();
+
+      expect(subject.observed).toBeFalsy();
+    });
   });
 });
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,12 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription, first } from 'rxjs';
+
+import { PlayerService } from './player.service';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss'],
 })
-export class PlayerComponent implements OnInit {
-  constructor() {}
+export class PlayerComponent implements OnInit, OnDestroy {
+  subs = new Subscription();
 
-  ngOnInit(): void {}
+  constructor(public playerService: PlayerService, private activatedRoute: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.subs.add(
+      this.activatedRoute.paramMap.subscribe((paramMap) => {
+        const id = paramMap.get('id') ?? '';
+
+        this.playerService
+          .loadQuizee(id)
+          .pipe(first())
+          .subscribe({
+            error: () => {
+              this.router.navigate(['solve/404']);
+            },
+          });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
