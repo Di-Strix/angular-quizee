c5e39d9f9734c9b2d7948f15cd4229db1a6e294b
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-08-30 22:36:07 +0200

    update `SeveralTrue` and `WriteAnswer` components
    update `SeveralTrue` and `WriteAnswer` components to implement `QuestionComponent`

--- src/app/player/question-screen/several-true/several-true.component.ts
+++ src/app/player/question-screen/several-true/several-true.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AnswerOptionId, Question } from '@di-strix/quizee-types';
+
+import { QuestionComponent } from '../question-component.type';
 
 @Component({
   selector: 'app-several-true',
   templateUrl: './several-true.component.html',
   styleUrls: ['./several-true.component.scss'],
 })
-export class SeveralTrueComponent implements OnInit {
-  constructor() {}
+export class SeveralTrueComponent implements QuestionComponent {
+  @Input() question!: Question;
+  @Output() answer = new EventEmitter<AnswerOptionId[]>();
 
-  ngOnInit(): void {}
+  constructor() {}
 }
--- src/app/player/question-screen/write-answer/write-answer.component.ts
+++ src/app/player/question-screen/write-answer/write-answer.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AnswerOptionId, Question } from '@di-strix/quizee-types';
+
+import { QuestionComponent } from '../question-component.type';
 
 @Component({
   selector: 'app-write-answer',
   templateUrl: './write-answer.component.html',
   styleUrls: ['./write-answer.component.scss'],
 })
-export class WriteAnswerComponent implements OnInit {
-  constructor() {}
+export class WriteAnswerComponent implements QuestionComponent {
+  @Input() question!: Question;
+  @Output() answer = new EventEmitter<AnswerOptionId[]>();
 
-  ngOnInit(): void {}
+  constructor() {}
 }
