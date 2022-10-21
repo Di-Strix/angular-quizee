5c60b2831d68871f52955ee8e40109c5de45e86f
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-10-21 19:41:53 +0200

    implement `SeveralTrue` component

--- src/app/player/question-screen/several-true/several-true.component.spec.ts
+++ src/app/player/question-screen/several-true/several-true.component.spec.ts
@@ -1,22 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SeveralTrueComponent } from './several-true.component';
 
 describe('SeveralTrueComponent', () => {
   let component: SeveralTrueComponent;
-  let fixture: ComponentFixture<SeveralTrueComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [SeveralTrueComponent],
-    }).compileComponents();
+    component = new SeveralTrueComponent();
 
-    fixture = TestBed.createComponent(SeveralTrueComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    jest.useFakeTimers();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('onInit', () => {
+    it('should subscribe to control value changes', async () => {
+      const sub = jest.spyOn(component.control.valueChanges, 'subscribe');
+
+      component.ngOnInit();
+
+      expect(sub).toBeCalledTimes(1);
+    });
+
+    it('should emit answer on control value change', async () => {
+      const emit = jest.spyOn(component.answer, 'emit');
+
+      component.ngOnInit();
+      component.control.setValue(['1']);
+      component.control.setValue(['2']);
+
+      await jest.runAllTimers();
+
+      expect(emit).toBeCalledTimes(2);
+      expect(emit).toHaveBeenNthCalledWith(1, ['1']);
+      expect(emit).toHaveBeenNthCalledWith(2, ['2']);
+    });
+  });
+
+  describe('toggleAnswer', () => {
+    it('should toggle answer', async () => {
+      expect(component.control.value).toEqual([]);
+
+      component.toggleAnswer('1');
+      expect(component.control.value).toEqual(['1']);
+
+      component.toggleAnswer('1');
+      expect(component.control.value).toEqual([]);
+
+      component.toggleAnswer('1');
+      component.toggleAnswer('2');
+      expect(component.control.value).toEqual(['1', '2']);
+
+      component.toggleAnswer('1');
+      expect(component.control.value).toEqual(['2']);
+    });
   });
 });
--- src/app/player/question-screen/several-true/several-true.component.ts
+++ src/app/player/question-screen/several-true/several-true.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { AnswerOptionId, Question } from '@di-strix/quizee-types';
+
+import * as _ from 'lodash';
 
 import { QuestionComponent } from '../question-component.type';
 
@@ -8,10 +11,22 @@ import { QuestionComponent } from '../question-component.type';
   templateUrl: './several-true.component.html',
   styleUrls: ['./several-true.component.scss'],
 })
-export class SeveralTrueComponent implements QuestionComponent {
+export class SeveralTrueComponent implements QuestionComponent, OnInit {
   @Input() question!: Question;
   @Output() answer = new EventEmitter<AnswerOptionId[]>();
   @Output() commit = new EventEmitter<void>();
 
+  control = new FormControl<AnswerOptionId[]>([], { nonNullable: true });
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.control.valueChanges.subscribe((answer) => {
+      this.answer.emit(answer);
+    });
+  }
+
+  toggleAnswer(id: AnswerOptionId) {
+    this.control.setValue(_.xor(this.control.value, [id]));
+  }
 }
