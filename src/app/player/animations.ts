d6fb631832a966d30bf356e3ebc3ced902bf5ca1
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-10-22 15:35:52 +0200

    export duration from `animations.ts`

--- src/app/player/animations.ts
+++ src/app/player/animations.ts
@@ -1,10 +1,12 @@
 import { animate, style } from '@angular/animations';
 
+export const ViewAnimationDuration = 500;
+
 export const ViewLeaveAnimation = [
   style({
     position: 'absolute',
   }),
-  animate('500ms ease-in-out', style({ transform: 'translateX(-20%)', filter: 'opacity(0)' })),
+  animate(`${ViewAnimationDuration}ms ease-in-out`, style({ transform: 'translateX(-20%)', filter: 'opacity(0)' })),
 ];
 
 export const ViewEnterAnimation = [
@@ -13,5 +15,5 @@ export const ViewEnterAnimation = [
     transform: 'translateX(20%)',
     filter: 'opacity(0)',
   }),
-  animate('500ms ease-in-out', style({ transform: 'translateX(0%)', filter: 'opacity(1)' })),
+  animate(`${ViewAnimationDuration}ms ease-in-out`, style({ transform: 'translateX(0%)', filter: 'opacity(1)' })),
 ];
