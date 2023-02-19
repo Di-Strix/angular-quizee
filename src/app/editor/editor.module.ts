c63a5bcf4c87fc0a01becebd0eb06bb3b97cbcff
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2023-02-19 21:57:24 +0100

    adapt `overview` component to mobile screens
    Since now overview component offers a convenient way of editing quizees on mobile devices.

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -76,6 +77,7 @@ import { SettingsComponent } from './settings/settings.component';
     MatDialogModule,
     MatStepperModule,
     PlayerModule,
+    MatExpansionModule,
   ],
   providers: [
     QuizeeEditingService,
